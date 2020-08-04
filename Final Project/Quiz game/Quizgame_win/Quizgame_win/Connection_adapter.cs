5b94a4d75e3797f328ae8d9cfa1410c0024178e4
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-08-04 15:46:14 +0500

    Create Connection_adapter.cs

---
+++ Final Project/Quiz game/Quizgame_win/Quizgame_win/Connection_adapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizgame_win
+{
+    class Connection_adapter : I_Connection
+    {
+        private SqlConnection Req = null;
+        public SqlConnection Request(SqlConnection req)
+        {
+
+            SqlConnection con = Connection_cls.Get(req);
+            return con;
+        }
+    }
+}

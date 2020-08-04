82bef24d0837026de8d8188a7aab501de968bf95
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-08-04 15:46:50 +0500

    Create I_Admin_Authent.cs

---
+++ Final Project/Quiz game/Quizgame_win/Quizgame_win/I_Admin_Authent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizgame_win
+{
+    interface I_Admin_Authent
+    {
+         string com1_text { get; set; }
+         int Login_ID { get; set; }
+         string Login_Pass { get; set; }
+         int check { get; set; }
+
+        int authentication();
+    }
+}

d28a7cede467b0db152c0ab27347a6a35e55bf76
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-08-04 15:46:17 +0500

    Create Connection_cls.cs

---
+++ Final Project/Quiz game/Quizgame_win/Quizgame_win/Connection_cls.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizgame_win
+{
+   public class Connection_cls 
+    {
+        private Connection_cls() { }
+
+        //singleton pattern
+        private static SqlConnection Sc = null;
+        public static SqlConnection Get()
+        {
+            if (Sc == null)
+            {
+                Sc = new SqlConnection();
+                Sc.ConnectionString = @"Data Source=AB-PC\;Initial Catalog=Dp_quiz;Integrated Security=True";
+                Sc.Open();
+
+            }
+            return Sc;
+
+        }
+        //adapter pattern
+
+
+        public static SqlConnection Get(SqlConnection sc)
+        {
+            Sc = sc;
+            if (Sc == null)
+            {
+                Sc = new SqlConnection();
+                Sc.ConnectionString = @"Data Source=AB-PC\;Initial Catalog=Dp_quiz;Integrated Security=True; Integrated Security=True";
+                Console.WriteLine("connnection Open ");
+                Sc.Open();
+
+            }
+            return Sc;
+
+        }
+
+
+    
+}
+}

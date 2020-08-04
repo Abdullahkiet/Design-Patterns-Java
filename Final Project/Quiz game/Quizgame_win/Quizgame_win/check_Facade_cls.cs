a4072f8549f50acd90c19256f1028fa1333aa044
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-08-04 15:46:08 +0500

    Create check_Facade_cls.cs

---
+++ Final Project/Quiz game/Quizgame_win/Quizgame_win/check_Facade_cls.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizgame_win
+{
+    class check_Facade_cls
+    {
+        I_QUIZ newQuiz = null;
+        public I_QUIZ Createnewquiz(string type)
+        {
+            switch (type)
+            {
+                case "Maths":
+                    break;
+                case "English":
+                    break;
+
+                default:
+                    break;
+            }
+            return newQuiz;
+
+        }
+    }
+}

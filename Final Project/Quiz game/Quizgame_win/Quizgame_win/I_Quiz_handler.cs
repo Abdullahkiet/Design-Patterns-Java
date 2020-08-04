a73088f243fe71138031236456aa2169436de347
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-08-04 15:47:01 +0500

    Create I_Quiz_handler.cs

---
+++ Final Project/Quiz game/Quizgame_win/Quizgame_win/I_Quiz_handler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizgame_win
+{
+   interface I_Quiz_handler
+    {
+        int Question_num { get; set; }
+        string Question { get; set; }
+         string option_a { get; set; }
+         string option_b { get; set; }
+         string option_c { get; set; }
+         string option_d { get; set; }
+        string correct_ans { get; set; }
+        void show_ques();
+    }
+}

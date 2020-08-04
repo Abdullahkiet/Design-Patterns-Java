32f06c2c9b4732ba90651a359528613f2c12d5ec
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-08-04 15:46:58 +0500

    Create I_QUIZ.cs

---
+++ Final Project/Quiz game/Quizgame_win/Quizgame_win/I_QUIZ.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizgame_win
+{
+    interface I_QUIZ
+    {
+    }
+}

58a4bbe100f70e0636545cca49c7317ae55d1656
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-08-04 15:48:31 +0500

    Create Questions_ran_cls.cs

---
+++ Final Project/Quiz game/Quizgame_win/Quizgame_win/Questions_ran_cls.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizgame_win
+{
+   public static class Questions_ran_cls
+    {
+
+        static bool Contains(int[] array, int value, int up)
+        {
+            for (int i = 0; i < up; i++)
+            {
+                if (array[i] == value) return true;
+            }
+            return false;
+        }
+
+
+        public static int[] get()
+        {
+
+
+            int[] x = new int[5];
+            Random r = new Random();
+
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                int next = 0;
+                while (true)
+                {
+                    next = r.Next(1, 10);
+
+                    if (Contains(x, next, i) == false)
+
+                        break;
+                }
+
+                x[i] = next;
+            }
+            return x;
+
+
+
+        }
+    }
+}
